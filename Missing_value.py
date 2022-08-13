1ecdc4737d67298946bb6e8790b5fd5d8267f22c
Arti <mishra.arti1010@gmail.com>
2022-08-13 18:57:50 +0530

    Create Missing_value.py

---
+++ Missing_value.py
@@ -0,0 +1,23 @@
+#An array of zize 1 to element N is given, like 1,2,3,4 till 10
+#we need to find the missing value in the array 
+class Solution:
+    def MissingNumber(self,array,n):
+        a=[]
+        b=[]
+        for i in range(1,n+1):
+            a.append(i)
+        for i in range(len(a)):
+            if a[i] not in array:
+                b.append(a[i])
+        return b
+                
+        
+n=int(input())
+array=[1,2,4,3,5,7,8,9,10]
+ob=Solution()
+
+print(ob.MissingNumber(array,n))
+
+#Missing value is = 4
+#TC:O(2n) - Lets try for O(n)
+#SC:O(1)
